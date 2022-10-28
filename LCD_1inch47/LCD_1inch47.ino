91b548338d1a61db5ddf95268e249fe8f67b4ab4
Yaman Kalaji <syk.yaman@gmail.com>
2022-10-29 00:34:55 +0100

    No enough memory space to include both happy & sad face... I have to draw the happy face programmatically :(

--- LCD_1inch47/LCD_1inch47.ino
+++ LCD_1inch47/LCD_1inch47.ino
@@ -9,30 +9,24 @@ void setup()
   LCD_Init();
   
   LCD_SetBacklight(100);
-  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, WHITE);
+  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, BLACK);
   Paint_Clear(BLACK);
     
-	Paint_DrawString_EN(30, 10, "123",        &Font24,  YELLOW, RED);  
-	Paint_DrawString_EN(30, 34, "ABC",        &Font24,  BLUE,   CYAN);
+	//Paint_DrawString_EN(30, 10, "123",        &Font24,  YELLOW, RED);  
+	//Paint_DrawString_EN(30, 34, "ABC",        &Font24,  BLUE,   CYAN);
   //Paint_SetMirroring(MIRROR_VERTICAL);
 //  Paint_DrawFloatNum (30, 58 ,987.654321,3, &Font20,  WHITE,  BLACK);
-// Paint_DrawString_CN(250,10, "P",   &Font24CN,WHITE,  RED);
-// Paint_DrawString_CN(250,40, "J",   &Font24CN,WHITE,  RED);
-// Paint_DrawString_CN(250,70, "K",   &Font24CN,WHITE,  RED);
-// Paint_DrawString_CN(250,100, "L",   &Font24CN,WHITE,  RED);
 
 //Paint_DrawRectangle(125, 10, 225, 58, RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawLine  (125, 10, 225, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
  //Paint_DrawLine  (225, 10, 125, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
- //Paint_DrawCircle(150,100,  25,        BLUE    ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
+ //Paint_DrawCircle(150,100,  95,        YELLOW    ,DOT_PIXEL_2X2,DRAW_FILL_FULL);
  //Paint_DrawCircle(180,100,  25,        BLACK   ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawCircle(210,100,  25,        RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawCircle(165,125,  25,        YELLOW  ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawCircle(195,125,  25,        GREEN   ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  
- Paint_DrawImage(gImage_happy, 36, 36, 100, 100); 
-
- Paint_DrawImage(gImage_sad, 150, 36, 100, 100); 
+ //Paint_DrawImage(gImage_sad, 36, 30, 115, 112);
  
 }
 void loop()
--- LCD_1inch47/image.h
+++ LCD_1inch47/image.h
@@ -1,7 +1,6 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 #include <avr/pgmspace.h>
-extern PROGMEM const unsigned char gImage_happy[];
 extern PROGMEM const unsigned char gImage_sad[];
 
 #endif
