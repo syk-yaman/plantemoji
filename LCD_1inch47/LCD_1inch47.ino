4f2d4acb599609b264efdfd78d387cf511a6497b
Yaman Kalaji <syk.yaman@gmail.com>
2022-10-29 21:15:17 +0100

    Read soil moisture level and display it on screen

--- LCD_1inch47/LCD_1inch47.ino
+++ LCD_1inch47/LCD_1inch47.ino
@@ -3,8 +3,12 @@
 #include "GUI_Paint.h"
 #include "image.h"
 
+int moisturePin = A5;
+
 void setup()
 {
+  Serial.begin(115200);
+   
   Config_Init();
   LCD_Init();
   
@@ -12,17 +16,17 @@ void setup()
   Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, BLACK);
   Paint_Clear(BLACK);
 
-  Paint_DrawImage(gImage_sad, 36, 30, 115, 112);
+  //Paint_DrawImage(gImage_sad, 36, 30, 115, 112);
    
 	//Paint_DrawString_EN(30, 10, "123",        &Font24,  YELLOW, RED);  
 	//Paint_DrawString_EN(30, 34, "ABC",        &Font24,  BLUE,   CYAN);
   //Paint_SetMirroring(MIRROR_VERTICAL);
 //  Paint_DrawFloatNum (30, 58 ,987.654321,3, &Font20,  WHITE,  BLACK);
 
-Paint_DrawRectangle(125, 10, 225, 58, RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
+//Paint_DrawRectangle(125, 10, 225, 58, RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawLine  (125, 10, 225, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
  //Paint_DrawLine  (225, 10, 125, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
- Paint_DrawCircle(220,86,  52,        NEWYELLOW    ,DOT_PIXEL_1X1,DRAW_FILL_FULL);
+ //Paint_DrawCircle(220,86,  52,        NEWYELLOW    ,DOT_PIXEL_1X1,DRAW_FILL_FULL);
  //Paint_DrawCircle(180,100,  25,        BLACK   ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawCircle(210,100,  25,        RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
  //Paint_DrawCircle(165,125,  25,        YELLOW  ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
@@ -32,7 +36,10 @@ Paint_DrawRectangle(125, 10, 225, 58, RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
 }
 void loop()
 {
-  
+    float sensorValue = analogRead(moisturePin);
+    Serial.println(sensorValue);
+    Paint_DrawFloatNum (260, 58 ,sensorValue,3, &Font20,  WHITE,  BLACK);
+    delay(1000);
 }
 
 
