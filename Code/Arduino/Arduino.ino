38641199eec6baaaa3438607add64d804d6de5c5
Yaman Kalaji <syk.yaman@gmail.com>
2022-10-31 01:55:33 +0000

    Remove LCD code from Arduino because I decided to connect it to Huzzah instead

--- Code/Arduino/Arduino.ino
+++ Code/Arduino/Arduino.ino
@@ -1,50 +1,14 @@
-#include <SPI.h>
-#include "LCD_Driver.h"
-#include "GUI_Paint.h"
-#include "image.h"
-
 int moisturePin = A5;
 
 void setup()
 {
   Serial.begin(115200);
-   
-  Config_Init();
-  LCD_Init();
-  
-  LCD_SetBacklight(100);
-  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, BLACK);
-  Paint_Clear(BLACK);
 
-  //Paint_DrawImage(gImage_sad, 36, 30, 115, 112);
-   
-	//Paint_DrawString_EN(30, 10, "123",        &Font24,  YELLOW, RED);  
-	//Paint_DrawString_EN(30, 34, "ABC",        &Font24,  BLUE,   CYAN);
-  //Paint_SetMirroring(MIRROR_VERTICAL);
-//  Paint_DrawFloatNum (30, 58 ,987.654321,3, &Font20,  WHITE,  BLACK);
-
-//Paint_DrawRectangle(125, 10, 225, 58, RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
- //Paint_DrawLine  (125, 10, 225, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
- //Paint_DrawLine  (225, 10, 125, 58,    MAGENTA ,DOT_PIXEL_2X2,LINE_STYLE_SOLID);
- //Paint_DrawCircle(220,86,  52,        NEWYELLOW    ,DOT_PIXEL_1X1,DRAW_FILL_FULL);
- //Paint_DrawCircle(180,100,  25,        BLACK   ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
- //Paint_DrawCircle(210,100,  25,        RED     ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
- //Paint_DrawCircle(165,125,  25,        YELLOW  ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
- //Paint_DrawCircle(195,125,  25,        GREEN   ,DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
- 
- 
 }
 void loop()
 {
     float soilMoistureValue = analogRead(moisturePin);
     String sensorData = String(soilMoistureValue) + ";" + String(soilMoistureValue) + ";" + String(1.0) + ";" + String(soilMoistureValue);
     Serial.println(sensorData);
-    //Paint_DrawFloatNum (260, 58 ,sensorValue,3, &Font20,  WHITE,  BLACK);
     delay(1000);
 }
-
-
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
--- Code/Arduino/image.h
+++
@@ -1,6 +0,0 @@
-#ifndef _IMAGE_H_
-#define _IMAGE_H_
-#include <avr/pgmspace.h>
-extern PROGMEM const unsigned char gImage_sad[];
-
-#endif
