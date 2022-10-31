ec91d50aefadffe242504d8feedc0e26a4bb21b9
Yaman Kalaji <syk.yaman@gmail.com>
2022-10-31 02:36:28 +0000

    Read LDR value from Arduino and send it to Huzzah

--- Code/Arduino/Arduino.ino
+++ Code/Arduino/Arduino.ino
@@ -1,14 +1,17 @@
-int moisturePin = A5;
+const int moisturePin = A5;
+const int ldrPin = A4;
 
 void setup()
 {
   Serial.begin(115200);
-
+  pinMode(ldrPin, INPUT);
+  pinMode(moisturePin, INPUT);
 }
 void loop()
 {
     float soilMoistureValue = analogRead(moisturePin);
-    String sensorData = String(soilMoistureValue) + ";" + String(soilMoistureValue) + ";" + String(1.0) + ";" + String(soilMoistureValue);
+    float ldrValue = analogRead(ldrPin);
+    String sensorData = String(soilMoistureValue) + ";" + String(1.0) + ";" + String(1.0) + ";" + String(ldrValue);
     Serial.println(sensorData);
     delay(1000);
 }
