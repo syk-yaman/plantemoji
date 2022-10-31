134ead44445e2a0811b88dd0296fc5c0f88ad9f3
Yaman Kalaji <syk.yaman@gmail.com>
2022-10-31 03:42:28 +0000

    Read humidity & temperature values from Arduino and send them to Huzzah

--- Code/Arduino/Arduino.ino
+++ Code/Arduino/Arduino.ino
@@ -1,17 +1,27 @@
 const int moisturePin = A5;
 const int ldrPin = A4;
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(ldrPin, INPUT);
   pinMode(moisturePin, INPUT);
+  dht.begin();
 }
 void loop()
 {
     float soilMoistureValue = analogRead(moisturePin);
     float ldrValue = analogRead(ldrPin);
-    String sensorData = String(soilMoistureValue) + ";" + String(1.0) + ";" + String(1.0) + ";" + String(ldrValue);
+    float humidityValue = dht.readHumidity();
+    float temperatureValue= dht.readTemperature();
+    
+    String sensorData = String(soilMoistureValue) + ";" + String(temperatureValue) + ";" + String(humidityValue) + ";" + String(ldrValue);
     Serial.println(sensorData);
     delay(1000);
 }
