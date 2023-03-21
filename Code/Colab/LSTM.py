e7f972e813030f00e10f2cbd643b5629a7c2d0cd
Yaman Kalaji <syk.yaman@gmail.com>
2023-03-21 17:41:58 +0000

    Complete model training and do some prediction

--- Code/Colab/LSTM.py
+++ Code/Colab/LSTM.py
@@ -1,3 +1,5 @@
+#This code is based on https://github.com/google-coral/tutorials/blob/master/train_lstm_timeseries_ptq_tf2.ipynb
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -117,8 +119,8 @@ df2 = pd.DataFrame({'airHumidity':airHumidityDataset, 'airTemperature':airTemper
 df2.dtypes
 df2 = df2.apply(pd.to_numeric)
 
-def show_heatmap(data):
-    plt.matshow(data.corr())
+def show_heatmap(data, method):
+    plt.matshow(data.corr(method))
     plt.xticks(range(data.shape[1]), data.columns, fontsize=14, rotation=90)
     plt.gca().xaxis.tick_bottom()
     plt.yticks(range(data.shape[1]), data.columns, fontsize=14)
@@ -127,6 +129,161 @@ def show_heatmap(data):
     plt.title("Feature Correlation Heatmap", fontsize=14)
     plt.show()
 
-show_heatmap(df2)
+show_heatmap(df2, 'pearson')
 
 
+split_fraction = 0.715
+train_split = int(split_fraction * int(df.shape[0]))
+step = 50
+
+past = 12000
+future = 4000
+learning_rate = 0.001
+batch_size = 256
+
+def normalize(data, train_split):
+    data_mean = data[:train_split].mean(axis=0)
+    data_std = data[:train_split].std(axis=0)
+    return (data - data_mean) / data_std
+    
+print(
+    "The selected parameters are:",
+    ", ".join([titles[i] for i in [0, 1, 2, 3, 4]]),
+)
+selected_features = [feature_keys[i] for i in [0, 1, 2, 3, 4]]
+features = df2[selected_features] #using the corrected dtypes
+features.index = df[date_time_key]
+features.head()
+
+features = normalize(features.values, train_split)
+features = pd.DataFrame(features)
+features.head()
+
+train_data = features.loc[0 : train_split - 1]
+val_data = features.loc[train_split:]
+
+
+start = past + future
+end = start + train_split
+
+x_train = train_data[[i for i in range(5)]].values
+y_train = features.iloc[start:end][[1]]
+print('training size:', len(x_train))
+
+sequence_length = int(past / step)
+print('window size:', sequence_length)
+
+dataset_train = keras.preprocessing.timeseries_dataset_from_array(
+    x_train,
+    y_train,
+    sequence_length=sequence_length,
+    sampling_rate=step,
+    batch_size=batch_size,
+)
+
+####
+
+x_end = len(val_data) - past - future
+
+label_start = train_split + past + future
+
+x_val = val_data.iloc[:x_end][[i for i in range(5)]].values
+y_val = features.iloc[label_start:][[1]]
+
+dataset_val = keras.preprocessing.timeseries_dataset_from_array(
+    x_val,
+    y_val,
+    sequence_length=sequence_length,
+    sampling_rate=step,
+    batch_size=batch_size,
+)
+
+for batch in dataset_train.take(1):
+    inputs, targets = batch
+
+print("Input shape:", inputs.numpy().shape)
+print("Target shape:", targets.numpy().shape)
+
+inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
+lstm_out = keras.layers.LSTM(32)(inputs)
+outputs = keras.layers.Dense(1)(lstm_out)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss='mse')
+model.summary()
+
+epochs = 10
+
+path_checkpoint = "model_checkpoint.h5"
+es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
+
+modelckpt_callback = keras.callbacks.ModelCheckpoint(
+    monitor="val_loss",
+    filepath=path_checkpoint,
+    verbose=1,
+    save_weights_only=True,
+    save_best_only=True,
+)
+
+history = model.fit(
+    dataset_train,
+    epochs=epochs,
+    validation_data=dataset_val,
+    callbacks=[es_callback, modelckpt_callback],
+)
+
+def visualize_loss(history, title):
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    epochs = range(len(loss))
+    plt.figure()
+    plt.plot(epochs, loss, "b", label="Training loss")
+    plt.plot(epochs, val_loss, "r", label="Validation loss")
+    plt.title(title)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+    print("Final val loss: ", val_loss)
+
+visualize_loss(history, "Training and Validation Loss")
+
+dataset_test = keras.preprocessing.timeseries_dataset_from_array(
+    x_val,
+    y_val,
+    sequence_length=sequence_length,
+    sequence_stride=int(sequence_length * 6),
+    sampling_rate=step,
+    batch_size=1,
+)
+
+def show_plot(plot_data, delta, title):
+    labels = ["History", "True Future", "Model Prediction"]
+    marker = [".-", "rx", "go"]
+    time_steps = list(range(-(plot_data[0].shape[0]), 0))
+    if delta:
+        future = delta
+    else:
+        future = 0
+    plt.title(title)
+    for i, val in enumerate(plot_data):
+        if i:
+            plt.plot(future, plot_data[i], marker[i], markersize=10, label=labels[i])
+        else:
+            plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+    plt.legend()
+    plt.xlim([time_steps[0], (future + 5) * 2])
+    plt.xlabel("Time-Step")
+    plt.show()
+    return 
+
+for x, y in dataset_test.take(5):
+    prediction = model.predict(x)
+    prediction = prediction[0]
+    print('prediction:', prediction)
+    print('truth:', y[0].numpy())
+    show_plot(
+        [x[0][:, 1].numpy(), y[0], prediction],
+        12,
+        "Single Step Prediction",
+    )
